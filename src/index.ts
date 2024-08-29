b581616395f9ef7015446400c10a6c6cec486f0e
astromanrx <astromanrx@gmail.com>
2024-08-29 08:53:07 +0330

    monitor added.

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,16 @@ import { Server } from "colyseus";
 import { createServer } from "http";
 import express from "express";
 import { KartRoom } from "./KartRoom";
-// import { LobbyRoom } from "./LobbyRoom";
-import { LobbyRoom } from "colyseus";
 import { playground } from "@colyseus/playground";
+import { monitor } from "@colyseus/monitor";
 
 const port = Number(process.env.PORT || 2567);
 const app = express();
 
 app.use(express.static('client'));
 app.use("/playground", playground);
+app.use("/monitor", monitor());
+
 
 const server = createServer(app);
 const gameServer = new Server({
